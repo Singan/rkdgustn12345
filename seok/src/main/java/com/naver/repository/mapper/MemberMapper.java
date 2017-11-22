b931baed95c6bec1685bb07c543cafa576b5155a
KANG-PC\kang <rkdgustn77@naver.com>
2017-11-22 16:24:55 +0900

    ㅁㅁㅁ

--- seok/src/main/java/com/naver/repository/mapper/MemberMapper.java
+++ seok/src/main/java/com/naver/repository/mapper/MemberMapper.java
@@ -2,12 +2,5 @@ package com.naver.repository.mapper;
 
 public class MemberMapper {
 	private final static String NS = "com.naver.repository.mapper.memberMapper.";
-
-	private static SqlSession session;
-	
-	public MemberMapper() {
-		session = MyAppSqlConfig.getSqlSessionInstance();
-	}
-	
 	
 }
