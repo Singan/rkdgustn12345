73a352027f279e083d4fb6804abf3e6d5366a7e8
tjrtjdgml <sunghee119z@naver.com>
2017-11-22 16:59:58 +0900

    zz

--- seok/src/main/java/com/naver/diary/controller/DiaryController.java
+++ seok/src/main/java/com/naver/diary/controller/DiaryController.java
@@ -1,5 +1,15 @@
 package com.naver.diary.controller;
 
-public class DiaryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/diary")
+public class DiaryController {
+	@RequestMapping("/diary.do")
+	public String diary(){
+		System.out.println("들어왔따");
+		return "/diary/diary";
+	}
+	
 }
